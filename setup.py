64612c330804a9041421b21847054e37e2a5310a
Kong <kong@kong.(none)>
2013-02-20 12:12:46 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+import setuptools
+import sys
+
+setuptools.setup(
+    name="passport",
+    version="2013.2",
+    author="Kong",
+    description="sso project with cdit",
+    packages=['passport']
+)
